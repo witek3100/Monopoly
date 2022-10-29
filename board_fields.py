b3ebf0ea63f4fe629a9f213f11fed4701f3886c3
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-10-29 17:51:03 +0200

    .

--- board_fields.py
+++ board_fields.py
@@ -1,11 +1,13 @@
 from player import Player
-
+from button import Button
 
 class BoardField:
 
-    def __init__(self, name, pos):
+    def __init__(self, screen, name, pos):
         self.name = name
         self.position = pos
+        self.screen = screen
+        self.button = Button(self.screen, pos, (40, 100), "")
 
     def action(self, player):
         player.money += 10
--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import sys
 import pygame
+
+import boardFieldsData
 from button import Button
 from player import Player
-
+from board_fields import *
 
 class Game:
 
@@ -12,22 +14,31 @@ class Game:
         self.screen = screen
         self.players = [Player(self.screen, num) for num in range(1)]
         self.objects_to_display = []
+        self.board_fields = [BoardField(self.screen, bf[0], bf[1]) for bf in boardFieldsData.fields_data]
+        self.buttons = [Button(self.screen, (10, 10), (140, 70), "DICE")] + [bf.button for bf in self.board_fields]
+
 
     def game_loop(self):
         board = pygame.image.load("board.xcf")
-        button = Button(self.screen, (10, 10), (140, 70), "DICE")
         run = True
         while run:
             self.screen.fill((0, 100, 0))
             self.screen.blit(board, (300, 70))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
             for player in self.players:
                 player.draw()
-                if button.action():
+                if self.buttons[0].action():
                     self.objects_to_display += player.move()
+
             for object in self.objects_to_display:
                 self.screen.blit(object[0], object[1])
-            button.draw()
+
+            for button in self.buttons:
+                button.draw()
+                button.action()
+
             pygame.display.update()
